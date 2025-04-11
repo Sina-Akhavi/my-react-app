6f97474d1d9eafd676b1b57db01a4c5a81608e17
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-11 10:37:59 +0330

    put home page in a correct route

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Sidebar from './components/Sidebar.js';
-import MainPanel from './components/MainPanel.js';
+// import Sidebar from './components/Sidebar.js';
+// import MainPanel from './components/MainPanel.js';
 import './styles/app.css'; 
 import LoginPage from './components/LoginPage.js';
+import HomePage from './components/HomePage.js';
 
 const App = () => {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path='/login' element={<LoginPage />}/>
-                <Route></Route>
-                <Route></Route>
-                <Route></Route>
+                <Route path='/login' element={<LoginPage/>} />
+                <Route path='/' element={<HomePage/>} />
+                <Route/>
+                <Route/>
             </Routes>
         </BrowserRouter>
 
---
+++ src/components/HomePage.js
@@ -0,0 +1,14 @@
+import Sidebar from './Sidebar.js';
+import MainPanel from './MainPanel.js';
+
+
+function HomePage() {
+  return (
+    <div className="wrapper">
+        <Sidebar />
+        <MainPanel />
+    </div>
+  );
+}
+
+export default HomePage;
