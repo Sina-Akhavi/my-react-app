93a29ac2d4a3870b2fbdb385297580d1b5282320
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-18 11:48:50 +0330

    added carousel to first page

--- src/components/HomePageContent.js
+++ src/components/HomePageContent.js
@@ -1,23 +1,69 @@
-import LineChart from './LineChart.js';
+import LineChart from "./LineChart.js";
+import ImageCarousel from "./ImageCarousel.js";
 
 function HomePageContent() {
-  const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+  const labels = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+  ];
   const values = [65, 59, 80, 120, 31, 55, 40];
-  
+  const images = require
+    .context("../assets/img", false, /\.webp$/)
+    .keys()
+    .map((file) => require(`../assets/img/${file.replace("./", "")}`));
+
   return (
-    <div className="content">
-      <div className="row">
-        <LineChart labels={labels} values={values} graphHeight="60% !important" graphWidth="100% !important" dynamicClassName="first-chart-style"/>
-      </div>
-      <div className="row" style={{ display: "flex", justifyContent: "space-between", marginTop: "20px" }}>
-        <div style={{ width: "48%" }}>
-          <LineChart labels={labels} values={values} graphHeight="100% !important" graphWidth="100% !important" dynamicClassName="second-chart-style"/>
+    <>
+      
+      <div className="content">
+
+        <div className="row">
+          <ImageCarousel images={images} interval={10000}/>
         </div>
-        <div style={{ width: "48%" }}>
-          <LineChart labels={labels} values={values} graphHeight="100% !important" graphWidth="100% !important" dynamicClassName="thirs-chart-style"/>
+        <br></br>
+        <div className="row">
+          <LineChart
+            labels={labels}
+            values={values}
+            graphHeight="60% !important"
+            graphWidth="100% !important"
+            dynamicClassName="first-chart-style"
+          />
+        </div>
+        <div
+          className="row"
+          style={{
+            display: "flex",
+            justifyContent: "space-between",
+            marginTop: "20px",
+          }}
+        >
+          <div style={{ width: "48%" }}>
+            <LineChart
+              labels={labels}
+              values={values}
+              graphHeight="100% !important"
+              graphWidth="100% !important"
+              dynamicClassName="second-chart-style"
+            />
+          </div>
+          <div style={{ width: "48%" }}>
+            <LineChart
+              labels={labels}
+              values={values}
+              graphHeight="100% !important"
+              graphWidth="100% !important"
+              dynamicClassName="thirs-chart-style"
+            />
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 }
 
