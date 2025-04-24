0418cf288a98378f51f193f77a5566ff074a2c08
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-04-24 23:06:50 +0330

    corrected url

--- src/components/RegisterPage.js
+++ src/components/RegisterPage.js
@@ -25,7 +25,7 @@ function RegisterPage() {
         }
 
         try {
-            const response = await fetch('/api/register', {
+            const response = await fetch('http://127.0.0.1:8000/api/auth/register/', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
