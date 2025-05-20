21c9dc7ddb7a97ce57d7abb668fe85b163fb86f7
Sina Akhavi <sinaakhavispecial1984@gmail.com>
2025-05-20 22:51:53 +0330

    designed analytics cards

---
+++ src/components/AnalyticsCardsContent.js
@@ -0,0 +1,65 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../styles/analytics-cards-content.css";
+import Footer from "./Footer";
+
+function AnalyticsCardsContent() {
+  return (
+    <>
+      <section className="analytics-cards-image">
+        <div className="analytics-overlay">
+          <h1>Evaluation, Forecasting and Comparison</h1>
+          <p>Utilize ARIMA and LSTM models to evaluate and forecast Bitcoin.</p>
+        </div>
+      </section>
+
+      <section className="cards-section">
+        <div className="cards-row">
+          <Link to="#" className="card-link">
+            <div className="analytic-card">
+              <i className="fas fa-chart-line card-icon"></i>
+              <h2>ARIMA Evaluation</h2>
+            </div>
+          </Link>
+          <Link to="#" className="card-link">
+            <div className="analytic-card">
+              <i className="fas fa-brain card-icon"></i>
+              <h2>LSTM Evaluation</h2>
+            </div>
+          </Link>
+        </div>
+        <div className="cards-row">
+          <Link to="#" className="card-link">
+            <div className="analytic-card">
+              <i className="far fa-chart-bar card-icon"></i>
+              <h2>ARIMA Forecasting</h2>
+            </div>
+          </Link>
+          <Link to="#" className="card-link">
+            <div className="analytic-card">
+              <i className="fas fa-chart-bar card-icon"></i>
+              <h2>LSTM Forecasting</h2>
+            </div>
+          </Link>
+        </div>
+        <div className="cards-row">
+          <Link to="#" className="card-link">
+            <div className="analytic-card">
+              <i className="fas fa-balance-scale card-icon"></i>
+              <h2>ARIMA vs LSTM</h2>
+            </div>
+          </Link>
+          <Link to="#" className="card-link">
+            <div className="analytic-card">
+              <i className="fas fa-info-circle card-icon"></i>
+              <h2>About Forecasting Models</h2>
+            </div>
+          </Link>
+        </div>
+      </section>
+      <Footer/>
+    </>
+  );
+}
+
+export default AnalyticsCardsContent;
